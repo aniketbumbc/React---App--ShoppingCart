d7f0d11a07238267304a6edeb57bc76668c23c25
aniket <aniket.umbc@gmail.com>
2021-05-18 20:30:40 +0530

    List added

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
+import '../styles/productList.css';
 const ProductList = () => {
   return (
-    <div>
-      <h1> Product List </h1>
-    </div>
+    <ListGroup className='mt-4'>
+      <ListGroupItem className='d-flex'>
+        <strong style={{ marginRight: '40%' }}>Customer One</strong>
+        <div className='ml-auto'>
+          <Link className='btn btn-warning editbtn' to='/edit/1'>
+            Edit
+          </Link>
+          <Button color='danger'>Delete</Button>
+        </div>
+      </ListGroupItem>
+    </ListGroup>
   );
 };
 
